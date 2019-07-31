55d75b2e99b0468129dce810303d2a47665c66ba
Wez Furlong <wez@wezfurlong.org>
2019-07-31 09:54:44 -0700

    wincng: use newer DH API for Windows 8.1+
    Since Windows 1903 the approach used to perform DH kex with the CNG
    API has been failing.
    
    This commit switches to using the `DH` algorithm provider to perform
    generation of the key pair and derivation of the shared secret.
    
    It uses a feature of CNG that is not yet documented.  The sources of
    information that I've found on this are:
    
    * https://stackoverflow.com/a/56378698/149111
    * https://github.com/wbenny/mini-tor/blob/5d39011e632be8e2b6b1819ee7295e8bd9b7a769/mini/crypto/cng/dh.inl#L355
    
    With this change I am able to successfully connect from Windows 10 to my
    ubuntu system.
    
    Fixes: https://github.com/libssh2/libssh2/issues/388
    Refs: https://github.com/alexcrichton/ssh2-rs/issues/122

--- src/wincng.h
+++ src/wincng.h
@@ -99,6 +99,7 @@ struct _libssh2_wincng_ctx {
     BCRYPT_ALG_HANDLE hAlgAES_ECB;
     BCRYPT_ALG_HANDLE hAlgRC4_NA;
     BCRYPT_ALG_HANDLE hAlg3DES_CBC;
+    BCRYPT_ALG_HANDLE hAlgDH;
 };
 
 struct _libssh2_wincng_ctx _libssh2_wincng;
@@ -383,8 +384,22 @@ _libssh2_bn *_libssh2_wincng_bignum_init(void);
 /*
  * Windows CNG backend: Diffie-Hellman support
  */
+#ifdef BCRYPT_KDF_RAW_SECRET
+// BCRYPT_KDF_RAW_SECRET is available from Windows 8.1 and onwards
+#define LIBSSH2_USE_BCRYPT_DH 1
+#endif
 
+#if LIBSSH2_USE_BCRYPT_DH
+typedef struct {
+  // holds our private+public key components
+  BCRYPT_KEY_HANDLE dh_handle;
+  // records the parsed out modulus and generator parameters
+  // that are shared with the peer
+  BCRYPT_DH_PARAMETER_HEADER *dh_params;
+} _libssh2_dh_ctx;
+#else
 #define _libssh2_dh_ctx struct _libssh2_wincng_bignum *
+#endif
 #define libssh2_dh_init(dhctx) _libssh2_dh_init(dhctx)
 #define libssh2_dh_key_pair(dhctx, public, g, p, group_order, bnctx) \
         _libssh2_dh_key_pair(dhctx, public, g, p, group_order)
